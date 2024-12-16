efcac31aa87ac837c743bb0fa9dc928c7a75240b
IranildoAraujo <iranildo.ramos@yahoo.com.br>
2024-12-15 22:34:11 -0300

    feat: GRF-5 implementanção de segurança

--- src/main/java/com/granaflow/config/security/ResourceServerConfig.java
+++ src/main/java/com/granaflow/config/security/ResourceServerConfig.java
@@ -1,6 +1,7 @@
 package com.granaflow.config.security;
 
 import java.util.Base64;
+import java.util.List;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
@@ -13,6 +14,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -25,7 +29,9 @@ public class ResourceServerConfig {
 
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated() // Protege todos os endpoints
+		http
+		.cors(cors -> cors.configurationSource(corsConfigurationSource())) // Configuração CORS
+		.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated() // Protege todos os endpoints
 		).oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder())));
 		return http.build();
 	}
@@ -35,5 +41,29 @@ public class ResourceServerConfig {
 		byte[] decodedKey = Base64.getDecoder().decode(jwtSecretKey);
 		SecretKey originalKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, ALGORITMO_DECODIFICADO);
 		return NimbusJwtDecoder.withSecretKey(originalKey).build();
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration configuration = new CorsConfiguration();
+
+	    // Definir origens permitidas - substituir pelos domínios reais da sua aplicação
+	    configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://www.granaflow.local:8080"));
+
+	    // Métodos HTTP permitidos
+	    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
+
+	    // Cabeçalhos permitidos
+	    configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+
+	    // Exposição de cabeçalhos específicos (caso necessário)
+	    configuration.setExposedHeaders(List.of("Authorization"));
+
+	    // Configuração para credenciais (se necessário, ex: cookies, autenticação baseada em sessão)
+	    configuration.setAllowCredentials(true);
+
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+	    return source;
 	}
 }
